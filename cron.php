de58d30220390ac1a375c94b0671431243a2e8dc
Soulou <leo@unbekandt.eu>
2016-01-29 00:36:34 +0100

    More output

--- cron.php
+++ cron.php
@@ -1,6 +1,8 @@
 <?php
   require(__DIR__ . '/vendor/autoload.php');
 
+  echo "[CRON] Starting tasks scheduler\n";
+
   // Increment redis key every minute
   $inc_job = new \Cron\Job\ShellJob();
   $inc_job->setCommand('php inc.php');
@@ -16,9 +18,11 @@
   // Every 60 seconds, run the scheduler which will execute the tasks
   // which have to be started at the given minute.
   while(true) {
+    echo "[CRON] Running tasks\n";
     $report = $cron->run();
     while ($cron->isRunning()) { }
 
+    echo "[CRON] " . count($report->getReports()) . " tasks have been executed\n";
     foreach($report->getReports() as $job_report) {
       $output = $job_report->getOutput();
       foreach($output as $line) {
