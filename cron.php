69e0ecaa99bf657c63117a216ab2c6e2bb99661f
Soulou <leo@unbekandt.eu>
2016-01-29 00:58:05 +0100

    Rebuild cron object at each iteration

--- cron.php
+++ cron.php
@@ -3,21 +3,26 @@
 
   echo "[CRON] Starting tasks scheduler\n";
 
-  // Increment redis key every minute
-  $inc_job = new \Cron\Job\ShellJob();
-  $inc_job->setCommand('php inc.php');
-  $inc_job->setSchedule(new \Cron\Schedule\CrontabSchedule('*/2 * * * *'));
+  function build_cron() {
+    // Increment redis key every minute
+    $inc_job = new \Cron\Job\ShellJob();
+    $inc_job->setCommand('php inc.php');
+    $inc_job->setSchedule(new \Cron\Schedule\CrontabSchedule('*/2 * * * *'));
 
-  $resolver = new \Cron\Resolver\ArrayResolver();
-  $resolver->addJob($inc_job);
+    $resolver = new \Cron\Resolver\ArrayResolver();
+    $resolver->addJob($inc_job);
 
-  $cron = new \Cron\Cron();
-  $cron->setExecutor(new \Cron\Executor\Executor());
-  $cron->setResolver($resolver);
+    $cron = new \Cron\Cron();
+    $cron->setExecutor(new \Cron\Executor\Executor());
+    $cron->setResolver($resolver);
+    return $cron;
+  }
 
   // Every 60 seconds, run the scheduler which will execute the tasks
   // which have to be started at the given minute.
   while(true) {
+    $cron = build_cron();
+
     echo "[CRON] Running tasks\n";
     $report = $cron->run();
     while ($cron->isRunning()) { }
