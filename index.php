f37094fe2279d9af738870b076d070060bad4de8
Étienne Michon <etienne@scalingo.com>
2018-03-02 12:50:34 +0100

    Better styling

--- index.php
+++ index.php
@@ -7,32 +7,98 @@
   $counter = $redis->get('counter');
 ?>
 
-<html style="html{height:100%}">
+<!DOCTYPE html>
+<html lang="en">
   <head>
-    <title>Sample PHP with CRON application on Scalingo</title>
+    <title>Greetings from a sample PHP app built with a task scheduler!</title>
+    <meta content='website' property='og:type'>
+    <meta content='https://cdn.scalingo.com/homepage/assets/fb-33a6a93ddbf90bfdae57407481aa05a4.png' property='og:image'>
+    <meta content="Deploying your own PHP/cron app on Scalingo is as easy as pie: JUST CLICK THIS BUTTON! It will be up in less than 2 minutes!" property='og:description'>
     <meta charset="utf-8">
-    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <link rel="stylesheet" media="screen" href="https://fonts.googleapis.com/css?family=Lato:700">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,500" rel="stylesheet">
+    <style>
+      body { color: #373a3c; font-family: 'Roboto', sans-serif; font-size: 1rem; line-height: 1.5; font-weight: 300; }
+      .h1, .h2, .h3, .h4, .h5, .h6, h1, h2, h3, h4, h5, h6 {
+        color: inherit;
+        font-family: inherit;
+        font-weight: 500;
+        line-height: 1.1;
+        margin-bottom: 1rem;
+        margin-top: 0;
+      }
+      a { color: #0275d8; text-decoration: none; background-color: transparent; }
+      a:focus, a:hover { color: #014c8c; text-decoration: underline; text-decoration: none; }
+      a:focus { outline: thin dotted; outline-offset: -2px; }
+      .container { margin: 0 auto; text-align: center; }
+      .hero { padding: 3rem 1.5rem; }
+      .love { margin-bottom: 2rem; }
+      h1 { font-size: 2.5rem; margin-bottom: 2rem; }
+      h2 { font-size: 1.6rem; font-weight: 300; }
+      small { margin-top: 0; }
+      .btn {
+        overflow: visible;
+        text-transform: uppercase;
+        text-decoration: none;
+        margin: 1em 0;
+        -moz-user-select: none;
+        border-radius: 0.25rem;
+        cursor: pointer;
+        display: inline-block;
+        font-size: 1rem;
+        font-weight: 400;
+        line-height: 1.5;
+        padding: 0.375rem 1rem;
+        text-align: center;
+        vertical-align: middle;
+        white-space: nowrap;
+        background-color: #0275d8;
+        border-color: #0275d8;
+        color: #fff;
+        border-radius: 0.3rem;
+        font-size: 1.25rem;
+        line-height: 1.33333;
+        padding: 0.75rem 1.25rem;
+      }
+      .btn:hover, .btn:focus, .btn:active { background-color: #025aa5; border-color: #01549b; color: #fff; background-image: none; }
+      .btn:active:focus, .btn:active:hover { background-color: #014682; border-color: #01315a; color: #fff; }
+      .scalingo-logo { max-width: 100%; height: auto; width: 330px; }
+      .heart-logo { margin: 0 20px; width:56px;position:relative; top:-18px; }
+      .tech-logo { width:56px;position:relative;top:-18px; }
+      @media (min-width:768px){
+        .container { width: 800px; }
+        .one-liner { display: block; }
+      }
+      @media (max-width:767px){
+        .scalingo-logo, .heart-logo, .tech-logo { display: block; position: unset; margin: 0 auto; }
+        .scalingo-logo, .heart-logo { margin-bottom: 1rem; }
+      }
+    </style>
   </head>
-  <body style="-webkit-transform-style:preserve-3d;transform-style:preserve-3d;text-align:center;height:100%;margin:0;padding:0;background-color:black;background-image:url('background.jpg');no-repeat fixed center center/cover #000;color:white">
-    <a href="https://github.com/Scalingo/sample-php-cron">
-      <img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png" alt="Fork me on GitHub">
-    </a>
-    <div id="wrapper" style="position:relative;top:40%;-webkit-transform:translateY(-40%);-ms-transform:translateY(-40%);transform:translateY(-40%)">
-      <h1 style="font-size:2rem;font-weight:700;text-shadow:0 1px 2px rgba(0,0,0,0.5);font-family:'Lato', sans-serif;margin:0;paddding:0">This is a sample application written in PHP with a task scheduler</h1>
-      <br>
-      <p style="font-size:2rem;">CRON task has been started
-        <?php
-          echo $counter + "";
-        ?>
-        times
-      </p> 
-      <br>
-      <a style="margin-top:1rem;text-decoration:none" href="https://scalingo.com">
-        <img style="height:40px" src="scalingo.svg">
-      </a>
+  <body>
+    <div class="container hero">
+      <div class="love">
+        <img class="scalingo-logo" alt="Scalingo" src="https://scalingo.com/logo-lightgrey.svg">
+        <img class="heart-logo" alt="love" src="https://cdn.scalingo.com/documentation/heart.png">
+        <img class="tech-logo" alt="PHP" src="https://cdn.scalingo.com/homepage/assets/technologies/svg/ruby-85779959327de14f801ee56cd5615036.svg">
+      </div>
+      <h1>Greetings from a sample PHP app <br/> built with a task scheduler!</h1>
+      <h2>
+        <span style="display:block;"><span class="one-liner">Deploying your own PHP/cron app on Scalingo</span> is as easy as pie:</span>
+        <a class="btn" href="https://my.scalingo.com/deploy?source=https://github.com/Scalingo/sample-php-cron">Just click this button!</a>
+        <span style="display:block;">It will be up in less than 2 minutes!</span>
+      </h2>
+      <p>The code of this sample lives <a href="https://github.com/Scalingo/sample-php-cron">on GitHub</a>. You can find <a href="http://samples.scalingo.com">more samples here</a>.</p>
     </div>
+    <footer>
+      <div class="container">
+        <p>
+          <span class="one-liner"><a href="https://scalingo.com">Scalingo</a> is the best Platform as a Service on the market:</span>
+          no server to install, no dependencies to install, just push your code!
+        </p>
+      </div>
+    </footer>
   </body>
 </html>
-
