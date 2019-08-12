e272cb704a30c54ecc11f0b67afcd0f3277bbf77
fa <francisco.azevedo@advertbrands.com>
2019-08-12 16:57:56 +0100

    fix scalingo cron memory crash

--- cron.php
+++ cron.php
@@ -34,6 +34,27 @@
         echo "[CRON] " . $line;
       }
     }
+
+    // this is a infinite loop, memory from used variables should be tag for cleanup
+    // (otherwise your app will crash without memory)
+    unset( $cron );
+    unset( $report );
+    if( isset( $job_report ) ) {
+      unset( $job_report );
+    }
+    if( isset( $output ) ) {
+      unset( $output );
+    }
+    if( isset( $line ) ) {
+      unset( $line );
+    }
+
     sleep(60);
+
+    // force memory cleanup for previous tagged variables (by "unset")
+    // ( php garbage collector will not free memory automatically )
+    // gc_collect_cycles() should be defined after sleep()
+    gc_collect_cycles();
+
   }
 ?>
